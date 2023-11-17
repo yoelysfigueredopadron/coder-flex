94dfbe179786e27554b42107d333d086bd92ec54
Yoelys Figueredo Padrón <yfigueredo@globalnewsgroup.com>
2023-11-16 21:41:46 -0300

    Actualizando ejemplos clase07

--- clase07/tienda-online-dom/js/index.js
+++ clase07/tienda-online-dom/js/index.js
@@ -3,30 +3,30 @@ const containerProducts = document.getElementById('container-products');
 
 iconMenu.addEventListener('click', showMenu);
 
-function showMenu() {
-    let navbar = document.getElementById('myTopnav');
+// console.log(productos);
 
-    if (navbar.className === 'topnav') {
-        navbar.className += ' responsive';
+function showMenu() {
+    let navBar = document.getElementById('navigation-bar');
+
+    if (navBar.className === 'navigation-bar') {
+        navBar.className += ' responsive';
     } else {
-        navbar.className = 'topnav';
+        navBar.className = 'navigation-bar';
     }
 }
-
-console.log(productos);
 
 const renderizarProductos = () => {
     productos.forEach((producto) => {
         const divCard = document.createElement('div');
         divCard.classList.add('card');
         divCard.innerHTML = `
-			<img src="./img/${producto.img}" alt="${producto.nombre}" />
-			<h4>${producto.nombre}</h4>
-			<p>$${producto.precio}</p>
-			<a id=${producto.id} class="boton agregar-carrito" href="#">Agregar</a>
-        `;
+                                <img src="./img/${producto.img}" alt="${producto.nombre}" />
+                                <h4>${producto.nombre}</h4>
+                                <p>$${producto.precio}</p>
+                                <a id="${producto.id}" class="boton agregar-carrito" href="#">Agregar</a>
+                            `;
 
-        containerProducts.appendChild(divCard);
+        containerProducts.append(divCard);
     });
 };
 
